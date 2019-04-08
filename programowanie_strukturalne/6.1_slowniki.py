d09a766e0ac8dd861b78961886871de73792f37d
Student <student@swps.edu.pl>
2019-04-08 18:59:23 +0200

    zadanie domowe

--- programowanie_strukturalne/6.1_slowniki.py
+++ programowanie_strukturalne/6.1_slowniki.py
@@ -6,7 +6,8 @@ pracownik = {
 'miasto':'poznan',
 'wiek':12,
 'imionaDzieci':['Anna','Paweł'],
-'imionaRodzicow':['marek','halina']}
+'imionaRodzicow':['marek','halina']
+}
 print(pracownik)
 print(pracownik['imionaDzieci'])
 print(pracownik['imionaDzieci'][0])
@@ -14,3 +15,40 @@ print(pracownik['imionaDzieci'][0])
 pracownik['wzrost'] = 175
 pracownik['waga'] = 75
 print(pracownik)
+
+klucz = 'wiek'
+
+if klucz in pracownik:
+    del pracownik[klucz]
+    print('klucz został usunięty')
+else:
+    print(f'klucz {klucz}nie został usunieęty')
+print(pracownik)
+
+
+
+print()
+print(pracownik.keys())
+
+
+
+
+
+print(pracownik.values())
+print(list(pracownik.values()))
+print(pracownik.items())
+
+for value in pracownik.values():
+    print(value,end=', ')
+
+
+
+
+print()
+print()
+print()
+for value in pracownik.values():
+    print(value,end=', ')
+
+for key, value in pracownik.items():
+    print(key, value)
